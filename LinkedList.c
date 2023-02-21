f85e1c178e923e8d57cc4c94d589393a3becd43f
Jason Yau <jasonyau101@gmail.com>
2023-02-21 08:43:26 -0500

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -6,7 +6,7 @@ typedef struct node {
     struct node* next;
 } node;
 
-node* initHead(int data) {
+node* init_head(int data) {
     node* head = NULL;
     head = (node*)malloc(sizeof(node));
     head->value = data;
@@ -14,34 +14,67 @@ node* initHead(int data) {
     return head;
 }
 
+node* createNode(int data, node* current, node* next) {
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->value = data;
+    newNode->next = next;
+    current->next = newNode;
+    return newNode;
+}
+
 void print(node* head) {
-    node* next = head;
-    while(next->next != NULL) {
-        printf("%d -> ", next->value);
-        next = next->next;
+    node* current = head;
+    while(current->next != NULL) {
+        printf("%d -> ", current->value);
+        current = current->next;
     }
     printf("NULL\n");
 }
 
-void tailInsert(int data, node* head) {
-    node* next = head;
+void head_insert(int data, node* head) {
     if (head == NULL) {
         return;
     }
-    int i = 0;
-    while (next->next != NULL) {
-        next = next->next;
+    node* next = createNode(head->value, head, head->next);
+    head->value = data;
+    head->next = next;
+}
+
+void tail_insert(int data, node* head) {
+    if (head == NULL) {
+        return;
     }
-    node* link = (node*)malloc(sizeof(node));
-    link->value = data;
-    link->next = NULL;
-    next->next = link;
+    if (head->next == NULL) {
+        createNode(data, head, NULL);
+    }
+    node* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    createNode(data, current, NULL);
+}
+
+void delete_list(node* head) {
+    node* current = head;
+    node* next = NULL;
+    while(current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
 }
 
 int main() {
-    node *head = initHead(5);
-    tailInsert(20, head);
-    tailInsert(25, head);
-    tailInsert(27, head);
+    node* head = init_head(5);
+    tail_insert(20, head);
+    tail_insert(25, head);
+    tail_insert(27, head);
+    head_insert(50,head);
     print(head);
+    delete_list(head);
+    head = init_head(5);
+    tail_insert(20, head);
+    head_insert(99, head);
+    print(head);
+    delete_list(head);
 }
