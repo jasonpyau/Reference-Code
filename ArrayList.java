6db6fa495627acf5d2625702e52e3f8907d11009
Jason Yau <jasonyau101@gmail.com>
2023-03-29 16:03:56 -0400

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,31 +1,59 @@
-public class ArrayList {
-    private int [] array;
+import java.lang.StringBuilder;
+
+public class ArrayList<T> {
+    private Object[] array;
     private int size;
     private int capacity;
-    private static final int DEFAULT_SIZE = 16;
+    private static final int DEFAULT_SIZE = 10;
     
     public ArrayList() {
-        this.size = 0;
-        this.capacity = DEFAULT_SIZE;
-        this.array = new int[capacity];
+        this(DEFAULT_SIZE);
     }
     
-    public void add(int value) {
+    public ArrayList(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        this.array = new Object[capacity];
+    }
+    
+    public void add(T value) {
+        add(size, value);
+    }
+    
+    public void add(int index, T value) {
         if (size >= capacity) {
             expandList();
         }
-        array[size] = value;
+        if (index < 0 || index > size) {
+            printError();
+            return;
+        }
+        for (int i = size; i > index; i--) {
+            array[i] = array[i-1];
+        }
+        array[index] = value;
         this.size++;
     }
     
-    public int get(int index) {
-        if (index >= 0 && index < size)
-            return array[index];
-        printError();
-        return 0;
+    public void addAll(T[] newArr) {
+        addAll(size, newArr);
     }
     
-    public void set(int index, int value) {
+    public void addAll(int index, T[] newArr) {
+        for (int i = 0; i < newArr.length; i++) {
+            add(index+i, newArr[i]);
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        if (index >= 0 && index < size)
+            return (T)array[index];
+        printError();
+        return null;
+    }
+    
+    public void set(int index, T value) {
         if (index < 0 || index >= size) {
             printError();
             return;
@@ -33,12 +61,13 @@ public class ArrayList {
             array[index] = value;
     }
     
-    public int remove(int index) {
+    @SuppressWarnings("unchecked")
+    public T remove(int index) {
         if (index < 0 || index >= size) {
             printError();
-            return 0;
+            return null;
         }
-        int removedItem = array[index];
+        T removedItem = (T)array[index];
         if (index == capacity-1) {
             this.size--;
             return removedItem;
@@ -50,11 +79,33 @@ public class ArrayList {
         return removedItem;
     }
     
-    public void print() {
-        System.out.print("[");
+    public void clear() {
+        this.size = 0;
+        this.capacity = DEFAULT_SIZE;
+        Object[] newArr = new Object[DEFAULT_SIZE];
+        this.array = newArr;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public T[] toArray(T[] arr) {
+        for (int i = 0; i < size && i < arr.length; i++) {
+            arr[i] = (T)array[i];
+        }
+        return arr;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
         for (int i = 0; i < size; i++) 
-            System.out.print(array[i] + ((i == size-1) ? "" : ", "));
-        System.out.println("]");
+            sb.append(((array[i] == null) ? "null" : array[i].toString()) + ((i == size-1) ? "" : ", "));
+        sb.append("]");
+        return sb.toString();
+    }
+    
+    public void print() {
+        System.out.println(this);
     }
     
     public int size() {
@@ -63,7 +114,7 @@ public class ArrayList {
     
     private void expandList() {
         capacity *= 2;
-        int [] newArr = new int[capacity];
+        Object[] newArr = new Object[capacity];
         for (int i = 0; i < size; i++)
             newArr[i] = array[i];
         this.array = newArr;
@@ -74,18 +125,46 @@ public class ArrayList {
     }
     
     public static void main(String[] args) {
-        ArrayList list = new ArrayList();
+        System.out.println("Integer ArrayList:");
+        ArrayList<Integer> list1 = new ArrayList<>();
         for (int i = 0; i <= 100; i++)
-            list.add(i);
-        list.set(50, 200);
-        list.set(100, 300);
-        System.out.println("Element at index 50: "+list.get(50));
-        System.out.println("Element at index 100: "+list.get(100));
-        list.print();
-        System.out.println("List size: "+list.size());
-        for(int i = list.size()-2; i >= 0; i -= 2)
-            list.remove(i);
-        list.print();
-        System.out.println("List size: "+list.size());
+            list1.add(i);
+        list1.set(50, 200);
+        list1.set(100, 300);
+        System.out.println("Element at index 50: "+list1.get(50));
+        System.out.println("Element at index 100: "+list1.get(100));
+        System.out.println(list1);
+        System.out.println("List size: "+list1.size());
+        for(int i = list1.size()-2; i >= 0; i -= 2)
+            list1.remove(i);
+        System.out.println(list1);
+        System.out.println("List size: "+list1.size());
+        
+        System.out.println("\nString ArrayList:");
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("Hello");
+        list2.add("World!");
+        System.out.println(list2);
+        System.out.println("List size: "+list2.size());
+        
+        System.out.println("\n2D String ArrayList:");
+        ArrayList<ArrayList<String>> list3 = new ArrayList<>();
+        list3.add(list2);
+        ArrayList<String> list4 = new ArrayList<>();
+        String[] array4 = {"ArrayList", "by", "Jason", "Yau"};
+        ArrayList<String> list5 = new ArrayList<>();
+        list3.add(list4);
+        list3.add(list5);
+        list4.addAll(array4);
+        list4.add(1, "implementation");
+        System.out.println(list3);
+        System.out.println("List size: "+list3.size());
+        list1.clear();
+        list2.clear();
+        list3.clear();
+        list4.clear();
+        list5.clear();
+        System.out.println(list3);
+        System.out.println("List size: "+list3.size());
     }
 }
