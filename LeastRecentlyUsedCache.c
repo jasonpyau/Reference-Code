19b60c5140cd950400e97ed5fa2d217287a769a6
Jason Yau <jasonyau101@gmail.com>
2023-04-10 12:37:00 -0400

    Create LeastRecentlyUsedCache.c

---
+++ LeastRecentlyUsedCache.c
@@ -0,0 +1,19 @@
+/**
+ * SOURCE CODE:
+ * https://github.com/jasonpyau/Leetcode-Solutions/blob/main/LRUCache.c
+ *
+ * Used for Leetcode Problem #146 https://leetcode.com/problems/lru-cache/description/
+ *
+ * Implementation:
+ * Each node has 4 pointers
+ * 2 pointers for Map linked list (Doubly). Map maps to a doubly linked list based on key
+ * 2 pointers for Deque linked list (Doubly)
+ * O(1) average ADD and PUT
+ *
+**/
+
+#include <stdio.h>
+
+int main () {
+  return 0;
+}
