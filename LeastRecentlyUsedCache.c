8c117d4758354eacfcd80f6e38323114ee9abd05
Jason Yau <jasonyau101@gmail.com>
2023-04-10 16:08:08 -0400

    Update LeastRecentlyUsedCache.c

--- LeastRecentlyUsedCache.c
+++ LeastRecentlyUsedCache.c
@@ -5,9 +5,10 @@
  * Used for Leetcode Problem #146 https://leetcode.com/problems/lru-cache/description/
  *
  * Implementation:
- * Each node has 4 pointers
- * 2 pointers for Map linked list (Doubly). Map maps to a doubly linked list based on key
- * 2 pointers for Deque linked list (Doubly)
+ * Each node has 3 pointers
+ * 1 pointers for Map linked list (Singly). Map maps to a singly linked list based on key
+ * On average, there will be no collision in map
+ * 2 pointers for Deque linked list (Doubly) keeping track of head & tail pointers
  * O(1) average ADD and PUT
  *
 **/
